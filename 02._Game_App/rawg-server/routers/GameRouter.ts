f2a5f5ec61cc539fab2fc99ec1e9fd93b380a4e5
realkoder <alexanderbtcc@gmail.com>
2025-03-03 23:17:10 +0100

    Added api client to work with configured rawg-server. Comments for game router

--- 02._Game_App/rawg-server/routers/GameRouter.ts
+++ 02._Game_App/rawg-server/routers/GameRouter.ts
@@ -5,6 +5,7 @@ import { Game } from "../entities/Game";
 import { Store } from "../entities/Store";
 import { ParentPlatform } from "../entities/ParentPlatform";
 
+//interface for response object matching what our rawg-client expects
 interface ModifinedGame {
   id: number;
   name: string;
@@ -24,14 +25,16 @@ const gameRouter = Router();
 const gameRepository = AppDataSource.getRepository(Game);
 
 gameRouter.get("/", async (req, res) => {
+  //query builder to get all games with their genres, parent_platforms, and stores
   const queryBuilder = gameRepository
     .createQueryBuilder("game")
     .leftJoinAndSelect("game.genres", "genres")
     .leftJoinAndSelect("game.parent_platforms", "parent_platforms")
     .leftJoinAndSelect("game.stores", "stores");
 
-  const games = await queryBuilder.getMany();
+  const games = await queryBuilder.getMany(); //execute query
 
+  //modifying the response object to match what our rawg-client expects
   const modifinedGames = games.map((game) => ({
     ...game,
     parent_platforms: game.parent_platforms?.map((parent_platform) => ({
